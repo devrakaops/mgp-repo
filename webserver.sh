b13bd6c53fef0b39640832e01113f403a22f722f
rakesh <rakesh@mail.com>
2025-11-25 20:59:15 +0530

    Version1.2 upgrade with playbook.yml & webserver.sh file

--- webserver.sh
+++ webserver.sh
@@ -5,3 +5,6 @@ systemctl restart httpd.service
 systemctl enable httpd.service
 
 firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --reload
+cp -arvf /root/website/*   /var/www/html
+echo "Webserver successfully setup"
